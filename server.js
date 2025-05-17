ad486c7127d574d1343dba05c0cdbfdeb171eb94
andremar93 <andremar93@gmail.com>
2025-05-17 15:36:59 -0300

    change expiration token

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.post('/login', async (req, res) => {
             return res.status(400).json({ error: 'Contraseña incorrecta' });
         }
 
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
 
         res.json({
             token,
